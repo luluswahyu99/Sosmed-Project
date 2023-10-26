c2dbe5a646bcb1dbe7a21f33b67c9d4e54666ef1
benisprlh <benisaprulah5@gmail.com>
2023-10-26 15:12:27 +0700

    update validate

--- controller/index.js
+++ controller/index.js
@@ -10,35 +10,50 @@ class Controller{
     }
 
     static registerForm(req, res){
-        res.render("register")
+        const {errors} = req.query;
+        let error;
+        if(errors){
+            error = errors.split(',')
+        }
+        res.render("register", {error})
     }
 
     static async registerAuth(req, res){
         const {username, email, password} = req.body;
+        
         try {
             const dataUser = await User.create({username, email, password})
             res.send("Login berhasil")
             // res.redirect(`/login`)
         } catch (error) {
-            console.log(error);
-            res.send(error.message)
+            if(error.name === "SequelizeValidationError"){
+                const errors = error.errors.map(item => {
+                    return item.message
+                })
+                res.redirect(`/register?errors=${errors}`)
+            }
+            
         }
     }
 
     static loginForm(req, res){
-        res.render("login")
+        const {msg} = req.query;
+        res.render("login", {msg})
     }
 
     static async loginVerification(req, res){
         const {username, password} = req.body;
+
         try {
             const verifUsername = await User.findOne({
                 where: {
                     username: username,
                 }
             })
-            if(!verifUsername) res.send("Username atau password salah");
+            const msg = "Username atau password salah"
+            if(!verifUsername) return res.redirect(`/login?msg=${msg}`);
             const verifPassword = bcrypt.compareSync(password, verifUsername.password)
+            if(!verifPassword) return res.redirect(`/login?msg=${msg}`);
             res.send("login berhasil")
         } catch (error) {
             console.log(error);
