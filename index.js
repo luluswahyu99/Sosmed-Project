993de7752eaa15b1a1898e2e2ea2b9729fb3388b
benisprlh <benisaprulah5@gmail.com>
2023-10-26 18:28:42 +0700

    updatecodejson

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const router = require('./routes')
 const app = express()
 const session = require('express-session')
 const port = 3000
+const multer = require('multer')
+const path = require('path')
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
+app.use('/home/', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use(session({
@@ -15,6 +35,7 @@ app.use(session({
     sameSite: true
    }
 }))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use(router)
 
 
